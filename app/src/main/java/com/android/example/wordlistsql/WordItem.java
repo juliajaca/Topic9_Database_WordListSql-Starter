aec671d2f531a57b9d38c184ae3438f332e8079b
jjaca <jjaca@cifpfbmoll.eu>
2022-02-18 21:14:04 +0100

    Initial commit

---
+++ app/src/main/java/com/android/example/wordlistsql/WordItem.java
@@ -0,0 +1,26 @@
+package com.android.example.wordlistsql;
+
+public class WordItem {
+    private int mId;
+    private String mWord;
+
+    public WordItem() {
+
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public String getWord() {
+        return mWord;
+    }
+
+    public void setWord(String word) {
+        mWord = word;
+    }
+}
